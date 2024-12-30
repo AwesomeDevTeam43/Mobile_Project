8bdc23a0228dec71d760d4845b314b24a7c0c24b
Jo6o43 <joaoribeiro141516@gmail.com>
2024-12-30 17:27:51 +0000

    Adjusting Bar Colors

--- app/src/main/java/com/example/mobile_project/Components/BottomBar.kt
+++ app/src/main/java/com/example/mobile_project/Components/BottomBar.kt
@@ -9,33 +9,38 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.mobile_project.ui.theme.Orange01
 
 @Composable
 fun BottomBar(navController: NavController) {
     BottomAppBar(
-        containerColor = MaterialTheme.colorScheme.primary
+        containerColor = Orange01
     ) {
         IconButton(onClick = { navController.navigate("home") }) {
             Icon(
                 imageVector = Icons.Default.Home,
-                contentDescription = "Home"
+                contentDescription = "Home",
+                tint = Color.White
             )
         }
         Spacer(modifier = Modifier.weight(1f)) // Espaçamento flexível para centralizar os ícones
         IconButton(onClick = { navController.navigate("favorites") }) {
             Icon(
                 imageVector = Icons.Default.Favorite,
-                contentDescription = "Favorites"
+                contentDescription = "Favorites",
+                tint = Color.White
             )
         }
         IconButton(onClick = { navController.navigate("profile") }) {
             Icon(
                 imageVector = Icons.Default.Person,
-                contentDescription = "Profile"
+                contentDescription = "Profile",
+                tint = Color.White
             )
         }
     }
--- app/src/main/java/com/example/mobile_project/Components/TopBar.kt
+++ app/src/main/java/com/example/mobile_project/Components/TopBar.kt
@@ -6,29 +6,33 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobile_project.ui.theme.Orange01
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(title: String) {
     TopAppBar(
-        title = { Text(text = title) },
+        title = { Text(text = title,color = Color.White) },
         actions = {
             IconButton(onClick = { /* Ação para pesquisar */ }) {
                 Icon(
                     imageVector = Icons.Default.Search,
-                    contentDescription = "Search"
+                    contentDescription = "Search",
+                    tint = Color.White
                 )
             }
             IconButton(onClick = { /* Ação para notificações */ }) {
                 Icon(
                     imageVector = Icons.Default.Notifications,
-                    contentDescription = "Notifications"
+                    contentDescription = "Notifications",
+                    tint = Color.White
                 )
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary
+            containerColor = Orange01
         )
     )
 }
